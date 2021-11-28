2d3a0e604d85ad86e2e25d4c35884a528b6cbbc7
nahodkins <tsuber2001@gmail.com>
2021-11-28 19:00:37 +0200

    Adds 1st task implementation

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,33 @@
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.DirectoryStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
 public class Main {
 
+    private static final String DIRECTORIES_DELIMITER = " -> ";
+    private static final String HOME_DIRECTORY_PROPERTY = "user.home";
+    private static final String CURRENT_DIRECTORY_PROPERTY = "user.dir";
+
+    public static String splitOnDirectories(Path path) {
+        return IntStream.range(0, path.getNameCount())
+                .mapToObj(path::getName)
+                .map(Path::toString)
+                .collect(Collectors.joining(DIRECTORIES_DELIMITER));
+    }
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Path home = Paths.get(System.getProperty(HOME_DIRECTORY_PROPERTY));
+        System.out.println("Home directory: " + home);
+        System.out.println(splitOnDirectories(home));
+        System.out.println();
+        Path current = Paths.get(System.getProperty(CURRENT_DIRECTORY_PROPERTY));
+        System.out.println("Current directory: " + current);
+        System.out.println(splitOnDirectories(current));
     }
 }
