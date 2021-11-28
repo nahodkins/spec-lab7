fce2a44c5c233f6b33af9c44c6e6b9242f2c1908
nahodkins <tsuber2001@gmail.com>
2021-11-28 20:09:48 +0200

    Adds 3rd task implementation

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,25 @@
+import java.io.IOException;
+import java.nio.file.DirectoryStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 public class Main {
 
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args) throws IOException {
+        if (args.length != 2) {
+            System.err.println("Wrong number of arguments!");
+            System.exit(1);
+        }
+
+        String directoryPath = args[0];
+        String startsWith = args[1];
+        Path path = Paths.get(directoryPath);
+        DirectoryStream<Path> directories = Files.newDirectoryStream(path);
+        for (Path directory : directories) {
+            if (directory.getFileName().toString().startsWith(startsWith)) {
+                System.out.println(directory.getFileName());
+            }
+        }
     }
 }
