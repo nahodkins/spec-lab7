81238647050f953cf7446cc5a8ecfb5af790a92c
nahodkins <tsuber2001@gmail.com>
2021-11-28 20:01:58 +0200

    Adds 2nd task implementation

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,20 @@
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+
 public class Main {
 
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    private static final String DESKTOP_DIR = System.getProperty("user.home") + File.separator + "Desktop";
+
+    public static void main(String[] args) throws IOException {
+        File tempFile = File.createTempFile("temp", ".tmp", new File(DESKTOP_DIR));
+        tempFile.deleteOnExit();
+        if (Files.exists(tempFile.toPath())) {
+            System.out.println("File was created!");
+            Files.writeString(tempFile.toPath(), "temp file content");
+            String fileContent = Files.readString(tempFile.toPath());
+            System.out.println("File content: " + fileContent);
+        }
+        System.in.read();
     }
 }
