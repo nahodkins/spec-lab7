56c6268cb6908143c54aabd5b024a1d9c2017267
nahodkins <tsuber2001@gmail.com>
2021-12-02 21:27:07 +0200

    Adds fix for the fourth task

--- src/main/java/FileCopierException.java
+++ src/main/java/FileCopierException.java
@@ -3,8 +3,4 @@ public class FileCopierException extends Throwable{
     public FileCopierException(String msg) {
         super(msg);
     }
-
-    public FileCopierException(Throwable cause) {
-        super(cause);
-    }
 }
